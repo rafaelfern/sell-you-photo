384601608c4078f05a7801af016c4851a530cf95
rafaelfern <gdcmf777@gmail.com>
2023-01-26 17:03:32 -0300

    Adjustment of types in boxFrame

--- src/components/BoxFrame/index.tsx
+++ src/components/BoxFrame/index.tsx
@@ -4,9 +4,9 @@ import styles from "./styles.module.scss";
 import Image from "next/image";
 
 const notoSans = Noto_Sans({
-	weight: "400",
-	variable: "--font-inter",
-	subsets: ["latin"],
+    weight: "400",
+    variable: "--font-inter",
+    subsets: ["latin"],
 });
 // interface IListItems {
 // 	listFrames: {
@@ -20,40 +20,40 @@ const notoSans = Noto_Sans({
 // }
 
 export default function index({ listFrames }: any) {
-	const getPriceByInstallments = (value, installments) =>
-		value / installments;
+    const getPriceByInstallments = (value: number, installments: number) =>
+        value / installments;
 
-	return (
-		<>
-			{listFrames.map((frame, i) => (
-				<div key={i} className={styles.boxContainer}>
-					<Image src={frame.image} alt="" width={250} height={159} />
-					<div className={styles.boxContent}>
-						<h4>{frame.title}</h4>
-						<span>
-							a partir de{" "}
-							<span className={notoSans.variable}>
-								R$ {frame.price}
-							</span>
-						</span>
-						<div className={styles.installmentsContent}>
-							ou
-							<span className={notoSans.variable}>
-								{frame.installments}X de R${" "}
-								{getPriceByInstallments(
-									frame.price,
-									frame.installments,
-								)}
-							</span>
-						</div>
-						<div className={styles.btnContent}>
-							<a href="" type="button">
-								Comprar
-							</a>
-						</div>
-					</div>
-				</div>
-			))}
-		</>
-	);
+    return (
+        <>
+            {listFrames.map((frame, i) => (
+                <div key={i} className={styles.boxContainer}>
+                    <Image src={frame.image} alt="" width={250} height={159} />
+                    <div className={styles.boxContent}>
+                        <h4>{frame.title}</h4>
+                        <span>
+                            a partir de{" "}
+                            <span className={notoSans.variable}>
+                                R$ {frame.price}
+                            </span>
+                        </span>
+                        <div className={styles.installmentsContent}>
+                            ou
+                            <span className={notoSans.variable}>
+                                {frame.installments}X de R${" "}
+                                {getPriceByInstallments(
+                                    frame.price,
+                                    frame.installments,
+                                )}
+                            </span>
+                        </div>
+                        <div className={styles.btnContent}>
+                            <a href="" type="button">
+                                Comprar
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </>
+    );
 }
