6dace817244205401a5d721bd5091c2a4eddfa08
rafaelfern <gdcmf777@gmail.com>
2023-01-27 10:52:57 -0300

    Changing head content

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 export default function Head() {
-  return (
-    <>
-      <title>Encontre sua foto</title>
-    </>
-  );
+    return (
+        <>
+            <title>Encontre sua foto na galeria</title>
+        </>
+    );
 }
