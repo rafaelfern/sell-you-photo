8c6cf3e10164f8977a23be64e1b201cd4b4268a2
rafaelfern <gdcmf777@gmail.com>
2023-01-27 09:44:29 -0300

    Adding husky pre commit

--- src/components/BoxFrame/index.tsx
+++ src/components/BoxFrame/index.tsx
@@ -8,52 +8,59 @@ const notoSans = Noto_Sans({
     variable: "--font-inter",
     subsets: ["latin"],
 });
-// interface IListItems {
-// 	listFrames: {
-// 		id: number;
-// 		seller: string;
-// 		price: string;
-// 		image: string;
-// 		title: string;
-// 		installments: string;
-// 	};
-// }
+interface IListItems {
+    listFrames: {
+        id: number;
+        seller: string;
+        price: string;
+        image: string;
+        title: string;
+        installments: string;
+    };
+}
 
-export default function index({ listFrames }: any) {
+export default function index({ listFrames }: IListItems[]) {
     const getPriceByInstallments = (value: number, installments: number) =>
         value / installments;
 
     return (
         <>
-            {listFrames.map((frame, i) => (
-                <div key={i} className={styles.boxContainer}>
-                    <Image src={frame.image} alt="" width={250} height={159} />
-                    <div className={styles.boxContent}>
-                        <h4>{frame.title}</h4>
-                        <span>
-                            a partir de{" "}
-                            <span className={notoSans.variable}>
-                                R$ {frame.price}
+            {listFrames.map((frame, i: number) => {
+                return (
+                    <div key={i} className={styles.boxContainer}>
+                        <Image
+                            src={frame.image}
+                            alt=""
+                            width={250}
+                            height={159}
+                        />
+                        <div className={styles.boxContent}>
+                            <h4>{frame.title}</h4>
+                            <span>
+                                a partir de{" "}
+                                <span className={notoSans.variable}>
+                                    R$ {frame.price}
+                                </span>
                             </span>
-                        </span>
-                        <div className={styles.installmentsContent}>
-                            ou
-                            <span className={notoSans.variable}>
-                                {frame.installments}X de R${" "}
-                                {getPriceByInstallments(
-                                    frame.price,
-                                    frame.installments,
-                                )}
-                            </span>
-                        </div>
-                        <div className={styles.btnContent}>
-                            <a href="" type="button">
-                                Comprar
-                            </a>
+                            <div className={styles.installmentsContent}>
+                                ou
+                                <span className={notoSans.variable}>
+                                    {frame.installments}X de R${" "}
+                                    {getPriceByInstallments(
+                                        frame.price,
+                                        frame.installments,
+                                    )}
+                                </span>
+                            </div>
+                            <div className={styles.btnContent}>
+                                <a href="" type="button">
+                                    Comprar
+                                </a>
+                            </div>
                         </div>
                     </div>
-                </div>
-            ))}
+                );
+            })}
         </>
     );
 }
